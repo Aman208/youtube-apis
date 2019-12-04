73449a41bfc87155183a047f0e465f06cae1b72b
Aman208 <43014406+Aman208@users.noreply.github.com>
2019-12-05 00:44:26 +0530

    remove api key

--- src/components/comments.js
+++ src/components/comments.js
@@ -2,4 +2,4 @@
 
   comments();
 
-  /* https://www.googleapis.com/youtube/v3/commentThreads?key=AIzaSyA09CFNFE7THRzzPmS08OARISn0i9ig0a0&textFormat=plainText&part=snippet&videoId=8AedZtuoVSg&maxResults=100 */
+  /* https://www.googleapis.com/youtube/v3/commentThreads?key=AIzaSyA09CFNn0i9ig0a0&textFormat=plainText&part=snippet&videoId=8AedZtuoVSg&maxResults=100 */
